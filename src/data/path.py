bc1abb0e37d4a3f5a20b252fbdcc77bc79d3ba00
BerserkerMother <anila.b218@yahoo.com>
2021-09-11 17:34:44 +0430

    ?

---
+++ src/data/path.py
@@ -0,0 +1,8 @@
+VQAv2_FILENAMES = {
+    'train_qu': 'questions/v2_OpenEnded_mscoco_train2014_questions.json',
+    'val_qu': 'questions/v2_OpenEnded_mscoco_val2014_questions.json',
+    'train_an': 'annotations/v2_mscoco_train2014_annotations.json',
+    'val_an': 'annotations/v2_mscoco_val2014_annotations.json',
+    'train_im': 'mscoco_imgfeat/train2014/',
+    'val_im': 'mscoco_imgfeat/val2014/'
+}
